63b2acf9eca2403d3dfb939499fcda9c0b88ade3
mariam-hamada <168047165+mariam-hamada@users.noreply.github.com>
2026-09-05 03:17:09 +0300

    Task 01 : Smart Product Search

--- Assignment11/Product.cs
+++ Assignment11/Product.cs
@@ -6,10 +6,22 @@ namespace Assignment11
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public double Price { get; set; }
-        public int Stock { get; set; }
+        public  int Id { get; set; }
+        public  string Name { get; set; }
+        public  string Category { get; set; }
+        public  double Price { get; set; }
+        public  int Stock { get; set; }
+
+        public static List<Product> SearchProducts(List<Product> products, Func<Product , bool> SearchDelegate)
+        {
+            // we use Func because it takes one paramenter of type product and return boolean , Predicate satisfies the conditions also 
+            List<Product> result = new List<Product>();
+            foreach (Product p in products) 
+            { 
+                if (SearchDelegate(p))
+                    result.Add(p);
+            }
+            return result;
+        }
     }
 }
